23dbace7af765d15c2192f142051d203df414866
Shobhit Nair <shobhit.210@gmail.com>
2021-10-10 14:31:56 +0530

    project commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-app';
+
+export class AppComponent{
+  phonenum=""
+  emailid=""
+
+  registerForm = this.fb.group({
+    phonenum: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+    emailid: ['', [Validators.required, Validators.email]]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+
 }
